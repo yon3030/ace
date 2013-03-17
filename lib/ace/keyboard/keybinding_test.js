yon3030/ace

commit 8634f2661ddec3380d000a33032af3f557f84cd2

update emacs mode

lib/ace/keyboard/keybinding.js
@@ -50,11 +50,12 @@ var KeyBinding = function(editor) {
     };
 
     this.setKeyboardHandler = function(kb) {
-        if (this.$handlers[this.$handlers.length - 1] == kb)
+        var h = this.$handlers;
+        if (h[h.length - 1] == kb)
             return;
 
-        while (this.$handlers[1])
-            this.removeKeyboardHandler(this.$handlers[1]);
+        while (h[h.length - 1] && h[h.length - 1] != this.defaultHandler)
+            this.removeKeyboardHandler(h[h.length - 1]);
 
         this.addKeyboardHandler(kb, 1);
     };

demo/kitchen-sink/demo.js
@@ -92,6 +92,12 @@ var cmdLine = new layout.singleLineEditor(consoleEl);
 cmdLine.editor = env.editor;
 env.editor.cmdLine = cmdLine;
 
+env.editor.showCommandLine = function(val) {
+    this.cmdLine.focus();
+    if (typeof val == "string")
+        this.cmdLine.setValue(val, 1);
+};
+
 /**
  * This demonstrates how you can define commands and bind shortcuts to them.
  */

lib/ace/editor.js
@@ -103,7 +103,9 @@ var Editor = function(renderer, session) {
      * 
      **/
     this.setKeyboardHandler = function(keyboardHandler) {
-        if (typeof keyboardHandler == "string" && keyboardHandler) {
+        if (!keyboardHandler) {
+            this.keyBinding.setKeyboardHandler(null);
+        } else if (typeof keyboardHandler == "string") {
             this.$keybindingId = keyboardHandler;
             var _self = this;
             config.loadModule(["keybinding", keyboardHandler], function(module) {
@@ -1279,16 +1281,15 @@ var Editor = function(renderer, session) {
         _numberRx.lastIndex = 0
 
         var s = this.session.getLine(row)
-        while(_numberRx.lastIndex < column - 1 ){
+        while (_numberRx.lastIndex < column) {
             var m = _numberRx.exec(s)
             if(m.index <= column && m.index+m[0].length >= column){
                 var number = {
                     value: m[0],
                     start: m.index,
                     end: m.index+m[0].length
-
                 }
-                return number
+                return number;
             }
         }
         return null;

lib/ace/keyboard/emacs.js
@@ -91,28 +91,29 @@ exports.handler.attach = function(editor) {
 
     editor.session.$emacsMark = null;
 
-    exports.markMode = function() {
-        return editor.session.$emacsMark;
+    editor.emacsMarkMode = function() {
+        return this.session.$emacsMark;
     }
 
-    exports.setMarkMode = function(p) {
-        editor.session.$emacsMark = p;
+    editor.setEmacsMarkMode = function(p) {
+        this.session.$emacsMark = p;
     }
 
-    editor.on("click",$resetMarkMode);
+    editor.on("click", $resetMarkMode);
     editor.on("changeSession",$kbSessionChange);
     editor.renderer.screenToTextCoordinates = screenToTextBlockCoordinates;
     editor.setStyle("emacs-mode");
     editor.commands.addCommands(commands);
     exports.handler.platform = editor.commands.platform;
+    editor.$emacsModeHandler = this;
 };
 
 exports.handler.detach = function(editor) {
     delete editor.renderer.screenToTextCoordinates;
     editor.session.$selectLongWords = $formerLongWords;
     editor.session.$useEmacsStyleLineStart = $formerLineStart;
-    editor.removeEventListener("click",$resetMarkMode);
-    editor.removeEventListener("changeSession",$kbSessionChange);
+    editor.removeEventListener("click", $resetMarkMode);
+    editor.removeEventListener("changeSession", $kbSessionChange);
     editor.unsetStyle("emacs-mode");
     editor.commands.removeCommands(commands);
 };
@@ -166,9 +167,10 @@ exports.handler.bindKey = function(key, command) {
 
 
 exports.handler.handleKeyboard = function(data, hashId, key, keyCode) {
+    var editor = data.editor;
     // insertstring data.count times
     if (hashId == -1) {
-        exports.setMarkMode(null);
+        editor.setEmacsMarkMode(null);
         if (data.count) {
             var str = Array(data.count + 1).join(key);
             data.count = null;
@@ -223,7 +225,7 @@ exports.handler.handleKeyboard = function(data, hashId, key, keyCode) {
         args = command.args;
         if (command.command) command = command.command;
         if (command === "goorselect") {
-            command = exports.markMode() ? args[1] : args[0];
+            command = editor.emacsMarkMode() ? args[1] : args[0];
             args = null;
         }
     }
@@ -232,9 +234,9 @@ exports.handler.handleKeyboard = function(data, hashId, key, keyCode) {
         if (command === "insertstring" ||
             command === "splitline" ||
             command === "togglecomment") {
-            exports.setMarkMode(null);
+            editor.setEmacsMarkMode(null);
         }
-        command = this.commands[command] || data.editor.commands.commands[command];
+        command = this.commands[command] || editor.commands.commands[command];
     }
 
     if (!command.readonly && !command.isYank)
@@ -330,10 +332,10 @@ exports.emacsKeys = {
     "C-/|C-x u|S-C--|C-z": "undo",
     "S-C-/|S-C-x u|C--|S-C-z": "redo", //infinite undo?
     // vertical editing
-    "C-x r":  "selectRectangularRegion"
-
+    "C-x r":  "selectRectangularRegion",
+    "M-x": {command: "focusCommandLine", args: "M-x "}
     // todo
-    // "M-x" "C-x C-t" "M-t" "M-c" "F11" "C-M- "M-q"
+    // "C-x C-t" "M-t" "M-c" "F11" "C-M- "M-q"
 };
 
 
@@ -365,21 +367,20 @@ exports.handler.addCommands({
         // "goto" commands become "select" commands.
         // Any insertion or mouse click resets mark-mode.
         // setMark twice in a row at the same place resets markmode
-        var markMode = exports.markMode();
+        var markMode = editor.emacsMarkMode();
         if (markMode) {
             var cp = editor.getCursorPosition();
             if (editor.selection.isEmpty() &&
-               markMode.row == cp.row && markMode.column == cp.column) {
-                exports.setMarkMode(null);
+                markMode.row == cp.row && markMode.column == cp.column) {
+                editor.setEmacsMarkMode(null);
                 // console.log("Mark mode off");
                 return;
             }
         }
         // turn on mark mode
         markMode = editor.getCursorPosition();
-        exports.setMarkMode(markMode);
+        editor.setEmacsMarkMode(markMode);
         editor.selection.setSelectionAnchor(markMode.row, markMode.column);
-
     },
     exchangePointAndMark: {
         exec: function(editor) {
@@ -407,7 +408,7 @@ exports.handler.addCommands({
         multiselectAction: "forEach"
     },
     killLine: function(editor) {
-        exports.setMarkMode(null);
+        editor.setEmacsMarkMode(null);
         var pos = editor.getCursorPosition();
         if (pos.column == 0 &&
             editor.session.doc.getLine(pos.row).length == 0) {
@@ -448,7 +449,11 @@ exports.handler.addCommands({
     },
     keyboardQuit: function(editor) {
         editor.selection.clearSelection();
-        exports.setMarkMode(null);
+        editor.setEmacsMarkMode(null);
+    },
+    focusCommandLine: function(editor, arg) {
+        if (editor.showCommandLine)
+            editor.showCommandLine(arg);
     }
 });
 

commit 4e2e516fefca3dfd2ce6d898c8483db7749f4ca3

fix for setKeyboardHandler bug introduced in 8634f26

lib/ace/keyboard/keybinding.js
@@ -54,7 +54,7 @@ var KeyBinding = function(editor) {
         if (h[h.length - 1] == kb)
             return;
 
-        while (h[h.length - 1] && h[h.length - 1] != this.defaultHandler)
+        while (h[h.length - 1] && h[h.length - 1] != this.$defaultHandler)
             this.removeKeyboardHandler(h[h.length - 1]);
 
         this.addKeyboardHandler(kb, 1);

lib/ace/keyboard/keybinding_test.js
@@ -1,0 +1,69 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Distributed under the BSD license:
+ *
+ * Copyright (c) 2010, Ajax.org B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Ajax.org B.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+if (typeof process !== "undefined") {
+    require("amd-loader");
+}
+
+define(function(require, exports, module) {
+"use strict";
+
+var EditSession = require("./../edit_session").EditSession,
+    Editor = require("./../editor").Editor,
+    MockRenderer = require("./../test/mockrenderer").MockRenderer,
+    assert = require("./../test/assertions"),
+    HashHandler = require('./hash_handler').HashHandler,
+    keys = require('../lib/keys'),
+    editor;
+
+function initEditor(docString) {
+    var doc = new EditSession(docString.split("\n"));
+    editor = new Editor(new MockRenderer(), doc);
+}
+
+module.exports = {
+
+    "test: adding a new keyboard handler does not remove the default handler": function() {
+        initEditor('abc');
+        var handler = new HashHandler({'del': 'f1'});
+        editor.keyBinding.setKeyboardHandler(handler);
+        editor.onCommandKey({}, 0, keys['f1']);
+        assert.equal('bc', editor.getValue(), "binding of new handler");
+        editor.onCommandKey({}, 0, keys['delete']);
+        assert.equal('c', editor.getValue(), "bindings of the old handler should still work");
+    }
+
+};
+
+});
+
+if (typeof module !== "undefined" && module === require.main) {
+    require("asyncjs").test.testcase(module.exports).exec()
+}

lib/ace/test/all_browser.js
@@ -22,6 +22,7 @@ var testNames = [
     "ace/editor_text_edit_test",
     "ace/ext/static_highlight_test",
     "ace/keyboard/emacs_test",
+    "ace/keyboard/keybinding_test",
     "ace/layer/text_test",
     "ace/lib/event_emitter_test",
     "ace/mode/coffee/parser_test",